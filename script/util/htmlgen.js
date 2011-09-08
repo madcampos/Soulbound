dadf763e20283a63ad03eb753bd764ff9f9a1e21
Marco Aurélio Dilda Campos <maurelio.campos@gmail.com>
2011-09-08 18:49:32 -0400

    namespace change and refactoring

--- script/util/htmlgen.js
+++ script/util/htmlgen.js
@@ -22,7 +22,18 @@ var HTMLGen = {
 			
 		}
 	},*/
-	createInput : function (inputType, inputValue, inputClass, inputId, description) {
+	
+	/**
+	 * Creates a <input> element with fallback wraped in a span
+	 * @param {String} inputType Expected input type, if none is provided default do "text"
+	 * @param {String} inputValue The input's value
+	 * @param {String} selectedClass The style to apply to selected inputs
+	 * @param {String} inputClass the input's class
+	 * @param {String} inputId The input's ID
+	 * @param {String} description The input's description (title)
+	 * @returns {HTMLSpanElement} An <span> element
+	 */
+	createInput : function (inputType, inputValue, selectedClass, inputClass, inputId, description) {
 		var wraper = document.createElement('span');
 		var backFace = document.createElement('input');
 		var input = document.createElement('input');
@@ -41,7 +52,16 @@ var HTMLGen = {
 		if (description) {
 			input.title = description;
 		}
-
+		
+		input.onfocus = function(event){
+			event.target.previousElementSibling.className += (' ' + selectedClass);
+		};
+		
+		input.onblur = function(event){
+			event.target.previousElementSibling.className.substring(0,
+				event.target.previousElementSibling.className.length - selectedClass.length);
+		};
+		
 		backFace.disabled = true;
 		wraper.appendChild(backFace);
 		
@@ -88,106 +108,93 @@ var HTMLGen = {
 				wraper.appendChild(input);
 		}
 		return wraper;
-	}
-};
-
-/**
- * Creates a <input> element
- * @param {String} inputType Expected input type, if none is provided default do "text"
- * @param {String} inputValue The input's value
- * @param {String} inputClass the input's class
- * @param {String} inputId The input's ID
- * @param {String} description The input's description (title)
- * @returns {HTMLSpanElement} An <input> element
- */
-
-
-/**
- * Creates a <select> element
- * @param {Array} options An array of <option> values
- * @param {Array} optionGroups An Array of <optgroup> values
- * @param {String} selected The selected option
- * @param {String} selectTd The id of the <select> element
- * @param {String} selectClass	The class of the <select> element
- * @param {Array} descriptions An Array containing descriptions (title attribute) for the <option> elements
- * @returns {HTMLSpanElement} A <select> element
- */
-function createSelect(options, optionGroups, selected, selectId, selectClass, descriptions) {
-	var select = document.createElement("select");
-
-	if (selectId) {
-		select.id = selectId;
-	}
-	if (selectClass) {
-		select.className = selectClass;
-	}
-	if (descriptions) {
-		select.onchange = selectChange();
-	}
-
-	if (optionGroups) {
-		for (var i in optionGroups) {
-			var optGroup = document.createElement("optgroup");
-			optGroup.label = optionGroups[i];
-
-			for (var j in options[i]) {
-				var opt = document.createElement("option");
-				if (typeof options[i][j] == "object") {
-					opt.textContent = options[i][j][0];
-					opt.className = options[i][j][1];
-				}else{
-					opt.textContent = options[i][j];
+	},
+	/**
+	 * Creates a <select> element
+	 * @param {Array} options An array of <option> values
+	 * @param {Array} optionGroups An Array of <optgroup> values
+	 * @param {String} selected The selected option
+	 * @param {String} selectTd The id of the <select> element
+	 * @param {String} selectClass	The class of the <select> element
+	 * @param {Array} descriptions An Array containing descriptions (title attribute) for the <option> elements
+	 * @returns {HTMLSpanElement} A <select> element
+	 */
+	createSelect : function(options, optionGroups, selected, selectId, selectClass, descriptions) {
+		var select = document.createElement("select");
+	
+		if (selectId) {
+			select.id = selectId;
+		}
+		if (selectClass) {
+			select.className = selectClass;
+		}
+		if (descriptions) {
+			select.onchange = selectChange();
+		}
+	
+		if (optionGroups) {
+			for (var i in optionGroups) {
+				var optGroup = document.createElement("optgroup");
+				optGroup.label = optionGroups[i];
+	
+				for (var j in options[i]) {
+					var opt = document.createElement("option");
+					if (typeof options[i][j] == "object") {
+						opt.textContent = options[i][j][0];
+						opt.className = options[i][j][1];
+					}else{
+						opt.textContent = options[i][j];
+					}
+	
+					if (opt.textContent == selected) {
+						opt.selected = "selected";
+						if (descriptions) {
+							select.title = descriptions[i][j];
+						}
+					}
+					if (descriptions) {
+						opt.title = descriptions[i][j];
+					}
+	
+					optGroup.appendChild(opt);
 				}
-
+	
+				select.appendChild(optGroup);
+			}
+		}else{
+			for (var i in options) {
+				var opt = document.createElement("option");
+				if (typeof options[i] == 'object') {
+					opt.textContent = options[i][0];
+					opt.className = options[i][1];
+				}else{
+					opt.textContent = options[i];
+				}
+	
 				if (opt.textContent == selected) {
 					opt.selected = "selected";
 					if (descriptions) {
-						select.title = descriptions[i][j];
+						select.title = descriptions[i];
 					}
 				}
 				if (descriptions) {
-					opt.title = descriptions[i][j];
+					opt.title = descriptions[i];
 				}
-
-				optGroup.appendChild(opt);
+	
+				select.appendChild(opt);
 			}
-
-			select.appendChild(optGroup);
 		}
-	}else{
-		for (var i in options) {
-			var opt = document.createElement("option");
-			if (typeof options[i] == 'object') {
-				opt.textContent = options[i][0];
-				opt.className = options[i][1];
-			}else{
-				opt.textContent = options[i];
+	
+		return select;
+	},
+	/**
+	 * Removes a HTML element
+	 * @param {String} nodeId The ID of the parent node
+	 */
+	removeElement : function(nodeId){
+			var elements = document.getElementById(nodeId);
+			if (elements.children.length > 0) {
+				elements.removeChild(elements.lastChild);
 			}
-
-			if (opt.textContent == selected) {
-				opt.selected = "selected";
-				if (descriptions) {
-					select.title = descriptions[i];
-				}
-			}
-			if (descriptions) {
-				opt.title = descriptions[i];
-			}
-
-			select.appendChild(opt);
-		}
 	}
-
-	return select;
-}
-
-/**
- * Removes a HTML element
- * @param {String} nodeId The ID of the parent node
- */
-function removeElements(nodeId){
-		var elements = document.getElementById(nodeId);
-		if (elements.children.length > 0) {
-			elements.removeChild(elements.lastChild);
-		}
-}
+};
